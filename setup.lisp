7567bed3092a10ccbd07cd7ed2f11cdca932713f
Zach Beane <xach@xach.com>
2014-01-20 12:15:08 -0500

    Updates to make directory setup work, maybe.

--- setup.lisp
+++ setup.lisp
@@ -3,6 +3,8 @@
 (in-package #:quicklisp-controller)
 
 (defun setup-directories (projects-pathname)
-  (sb-posix:mkdir #p "~/quicklisp-controller/" #o755)
-  (with-posix-cwd "~/quicklisp-controller/"
-    (sb-posix:symlink projects-pathname "projects")))
+  (let ((home #p "~/quicklisp-controller/"))
+    (unless (probe-file home)
+      (sb-posix:mkdir home #o755)
+      (with-posix-cwd home
+        (sb-posix:symlink projects-pathname "projects")))))
