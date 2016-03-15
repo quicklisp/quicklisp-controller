4dfa5696383ba988c594c17b6ad59530cfcea8e3
Zach Beane <xach@xach.com>
2016-03-15 15:03:45 -0400

    Add support for latest github tag.

--- upstream-github.lisp
+++ upstream-github.lisp
@@ -17,6 +17,16 @@
       (list :url (githappy:jref json '(0 "tarball_url"))
             :tag (githappy:jref json '(0 "tag_name"))))))
 
+(defun github-latest-tag-info (url)
+  (multiple-value-bind (owner repo)
+      (github-owner-and-repo url)
+    (unless (and owner repo)
+      (error "Could not parse ~A as a github repo url" url))
+    (let* ((response (githappy:repo-tags :owner owner :repo repo))
+           (json (and response (githappy:json response))))
+      (list :url (githappy:jref json '(0 "tarball_url"))
+            :tag (githappy:jref json '(0 "name"))))))
+
 
 (defclass latest-github-release-source (http-source)
   ((release-url
@@ -26,10 +36,25 @@
     :initarg :release-tag
     :accessor release-tag)))
 
-(defun initialize-release-info (source)
-  (let ((info (github-latest-release-info (location source))))
-    (setf (release-tag source) (getf info :tag)
-          (release-url source) (getf info :url))))
+(defclass latest-github-tag-source (latest-github-release-source)
+  ())
+
+(defgeneric github-info-plist (source)
+  (:method ((source symbol))
+    (let ((real-source (source-designator source)))
+      (unless real-source
+        (error "~S does not designate a source" source))
+      (github-info-plist real-source)))
+  (:method ((source latest-github-release-source))
+    (github-latest-release-info (location source)))
+  (:method ((source latest-github-tag-source))
+    (github-latest-tag-info (location source))))
+
+(defgeneric initialize-release-info (source)
+  (:method (source)
+    (let ((info (github-info-plist source)))
+      (setf (release-tag source) (getf info :tag)
+            (release-url source) (getf info :url)))))
 
 (defmethod slot-unbound ((class t) (source latest-github-release-source)
                          (slot-name (eql 'release-url)))
